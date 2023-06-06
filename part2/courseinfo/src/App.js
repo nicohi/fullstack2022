88bd18d23661d8f35106e20ed87cbdddb5f58e02
Nicolas Hiillos <nicolashiillos@gmail.com>
2023-06-06 17:52:07 +0300

    Complete 2.1 and 2.2

---
+++ part2/courseinfo/src/App.js
@@ -0,0 +1,53 @@
+const Header = ({ course }) => <h1>{course}</h1>
+
+const Total = ({ sum }) => <b>total of {sum} exercises</b>
+
+const Part = ({ part }) =>
+  <p>
+    {part.name} {part.exercises}
+  </p>
+
+const Content = ({ parts }) =>
+  <>
+      {parts.map(p => <Part part={p} key={p.id}/>)}
+  </>
+
+const Course = ({ course: { name, parts }}) =>
+  <div>
+    <Header course={name} />
+    <Content parts={parts} />
+    <Total sum={parts.reduce((acc,part) => acc + part.exercises, 0)} />
+  </div>
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      },
+      {
+        name: 'What is state really',
+        exercises: 144,
+        id: 4
+      }
+    ]
+  }
+
+  return <Course course={course} />
+}
+
+export default App
